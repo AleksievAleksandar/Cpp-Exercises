fb832d3fbd9458085856f0206d88f6055dda89e1
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-08-18 16:56:21 +0300

    Create Main.cpp

---
+++ 06. Exercises on Advanced Class Members and Code Organization/Task 4 - Divisible by 45/Main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "BigInt.h"
+#include <sstream>
+#include <vector>
+#include<cmath>
+
+bool operator != (const BigInt& a, const BigInt& b)
+{
+	return a.getDigits() != b.getDigits();
+}
+
+size_t digitSum(const BigInt& number) 
+{
+	size_t sum = 0;
+	std::string digits = number.getDigits();
+	for (size_t i = 0; i < digits.size(); i++) {
+		sum += digits[i] - '0';
+	}
+
+	return sum;
+}
+
+bool dividesBy9(const BigInt& number) 
+{
+	return digitSum(number) % 9 == 0;
+}
+
+bool dividesBy5(const BigInt& number) 
+{
+	std::string digits = number.getDigits();
+	int lastInd = digits.size() - 1;
+	return digits[lastInd] == '0' || digits[lastInd] == '5';
+}
+
+int main()
+{
+    std::string rangeStartStr;
+    std::string rangeEndStr;
+
+    std::cin >> rangeStartStr >> rangeEndStr;
+
+    BigInt rangeEnd(rangeEndStr);
+
+    BigInt firstInRange;
+    for (BigInt num = rangeStartStr; num < rangeEnd; num += 1) {
+        // if a number divides by both 5 and 9, then it divides by 5 * 9 = 45
+        if (dividesBy5(num) && dividesBy9(num)) {
+            firstInRange = num;
+            break;
+        }
+    }
+
+    for (BigInt num = firstInRange; num < rangeEnd; num += 45) {
+        std::cout << num << std::endl;
+    }
+
+    return 0;
+}
